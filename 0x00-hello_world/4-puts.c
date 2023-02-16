68415607be6eb8f381919e15f593fe0c9a45706b
Anthony Agbonoga <anthony.agbonoga@gmail.com>
2023-02-16 13:25:01 -0800

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with puts function
+ * main - A program that prints a line with puts function
  * Return : 0 (success)
  */
 int main(void)
