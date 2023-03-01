026d070ad68f6f7b18f6345afa51d87ffb3759bf
Anthony Agbonoga <anthony.agbonoga@gmail.com>
2023-03-01 11:38:30 -0800

    second

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -8,11 +8,13 @@
  */
 int main(void)
 {
-	char str[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	char str[] = "Expect the best. Prepare for the worst.\n
+		Capitalize on what comes.\n
+		"hello world !hello - world 0123456hello world\thello world.hello world\n";
 	char *ptr;
 
 	ptr = cap_string(str);
-	printf("%s", ptr);
-	printf("%s", str);
+	printf(" % s", ptr);
+	printf(" % s", str);
 	return (0);
 }
